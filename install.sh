cb13108f4a6afb64cae15d071f26ede2dd85cacd
rsvofc <161730170+rsvofc@users.noreply.github.com>
2025-01-27 23:17:13 +0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Color
+RED='\033[0;31m'
+NC='\033[0m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+LIGHT='\033[0;37m'
+clear
+echo -e "${GREEN}Bandwidth Limit Installer by RSV${NC}"
+sleep 5
+wget -q -O /usr/sbin/vpslimiter "https://raw.githubusercontent.com/rsvofc/tool/savina/wslimiter.sh"
+echo -e "${GREEN}Installed successfully!${NC}"
+echo -e "Ketik ${BLUE}[vpslimiter]${NC} untuk menjalankan"
+chmod +x /usr/sbin/wslimiter
+rm -rf install.sh
+exit 0
